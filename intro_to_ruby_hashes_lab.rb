fe2877185ad2414aebfa59dba1132568ff796d85
Nandita Venkat <nanditavenkat@gmail.com>
2020-04-08 17:30:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,8 +6,8 @@ end
 def monopoly_with_second_tier
  base={
    :railroads => {
-     :pieces => 4
-     :rent_in_dollars => {}
+     :pieces => 4,
+     :rent_in_dollars => {},
      :names => {}
    }
    
